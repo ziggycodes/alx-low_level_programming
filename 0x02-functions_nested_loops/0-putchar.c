5b832ed1db94a43d2c25cb6c39c943c0c4fa3c70
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-19 07:47:38 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * msin - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char *c = "_putchar";
+
+	while (*c)
+	{
+		_putchar(*c);
+		c++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
