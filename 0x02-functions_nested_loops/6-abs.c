4f033e843ca96e3887c2dbadfcb199f83a6835fb
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-19 11:54:51 +0000

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _abs - a function that computes the absolute value of an intefer
+ * _abs - a function that computes the absolute value of an integer
  * @ab: integer input
  * Return: absolute value of ab
  */
