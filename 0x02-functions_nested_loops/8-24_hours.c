8cfe453f1a55407f2deba09b0fdbad0f410769dd
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-19 23:59:42 +0000

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jack_bauer - function that prints every minute of the day, 24 hour clock
  * h = hour, m = minutes
- * /10 allows seconf digit to rotate
+ * / 10 allows second digit to rotate
  * for loop breaks before passing 24:00
  * Return: 24 hour clock line by line
  */
@@ -10,7 +10,7 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h = 24; h++)
+	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
