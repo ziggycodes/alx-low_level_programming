cc26a1eb697a62c8f19b1eb8f8ca8c00ead0bb47
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-19 08:32:44 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - afunction that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
