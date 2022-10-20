4e99938d38587b9dbc0a55f80f28de5582ede94f
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-20 00:19:39 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (cone = 1; cone <= 9; cone ++)
+		for (cone = 1; cone <= 9; cone++)
 		{
 			d = (rone * cone);
 			if ((d / 10) > 0)
@@ -26,7 +26,6 @@ void times_table(void)
 				_putchar(' ');
 			}
 			_putchar((d % 10) + '0');
-			
 			if (cone < 9)
 			{
 				_putchar(',');
