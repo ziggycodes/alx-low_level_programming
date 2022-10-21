52adf5204b1108934ec9b901e3c50a87d4470c05
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-21 00:33:46 +0000

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints numbers except 2 and 4
+ *
+ * Return: returns nothing
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n == 48; n < 50; n++)
+	{
+		if ((n == 50) || (n == 52))
+		{
+			continue;
+		}
+		putchar(0);
+	}
+	putchar(10);
+}
