5f0e0d3d9d0dd548798bc6dbce93cbea541aad4b
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-21 23:13:28 +0000

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that prints if interger is positiveor nebative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive or negative(i);
+
+	return (0);
+}
