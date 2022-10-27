20a26223a77635d78bb59c8eea5ad67a8af9aec4
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-27 22:54:33 +0000

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 char *leet(char *s)
 {
 	int count = 0, i;
-	int lower_case[] = {97, 101, 111, 116,108};
+	int lower_case[] = {97, 101, 111, 116, 108};
 	int upper_case[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
