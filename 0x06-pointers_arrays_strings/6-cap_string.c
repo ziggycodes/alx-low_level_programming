4755baf3af1ede91b20c8c000d41721f1fe5374e
Isaac Ayodele <ziggycodes25@gmail.com>
2022-10-27 23:00:18 +0000

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,8 @@ char *cap_string(char *s)
 	int count = 0, i;
 	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122) *(s + count) = *(s + count) - 32;
+	if (*(s + count) >= 97 && *(s + count) <= 122) 
+		*(s + count) = *(s + count) - 32;
 	count++;
 	while (*(s + count) != '\0')
 	{
@@ -19,7 +20,8 @@ char *cap_string(char *s)
 		{
 			if (*(s + count) == seperators[i])
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122)) *(s + (count + 1)) = *(s + (count + 1)) - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122)) 
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
