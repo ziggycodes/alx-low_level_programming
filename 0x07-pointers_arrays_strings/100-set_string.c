d3daeda35d990f0b52dfef1fedc1ce0d99a916e7
Isaac Ayodele <ziggycodes25@gmail.com>
2022-11-01 22:14:17 +0000

    task 199 and 101

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a vhst
+ * @s: content is to be copiec
+ * @to: string
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
