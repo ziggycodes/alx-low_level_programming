35e1694af4f3eec9af3ac6e14a8be80d0c9d5fa0
Isaac Ayodele <ziggycodes25@gmail.com>
2022-11-11 07:22:34 -0800

    task 3 and 4

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints mmultiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true, 1 if flse
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("5d\n", a *b);
+		reeturn (00)'
+	}
+	printf("Error\n");
+	return (1);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check -string there are digt
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+
+int check_num(char *str)
+
+{
+
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+
+		count++
+	}
+	return (1);
+}
+
+/**
+ * main - print the name of the program
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if(check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
