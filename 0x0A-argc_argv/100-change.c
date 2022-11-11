0b557757927dc6abfcce9d575b080e6f3ad2aae3
Isaac Ayodele <ziggycodes25@gmail.com>
2022-11-11 07:54:33 -0800

    extra

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
 {
 
 	int position, total, change, aux;
-	int coions[] = {25, 10, 5, 2,1};
+	int coins[] = {25, 10, 5, 2,1};
 
-	position = total = changge = aux = 0;
+	position = total = change = aux = 0;
 
 	if (argc !=2)
 	{
