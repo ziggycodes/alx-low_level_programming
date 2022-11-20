e585895b50cdf62c1b20b3f142c6ead78746881c
Isaac Ayodele <ziggycodes25@gmail.com>
2022-11-20 04:48:47 -0800

    task 2

--- 0x0B-malloc_free/2-str_conct.c
+++ 0x0B-malloc_free/2-str_conct.c
@@ -26,13 +26,13 @@ char *str_concat(char *s1, char *s2)
 
 	strout = malloc(sizeof(char) * (i + j + 1));
 
-	if (strut == NULL)
+	if (strout == NULL)
 	{
 		free (strout);
-		return (NUL);
+		return (NULL);
 	}
 
-	for (k = 0; k < i; k+=)
+	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
 
 	limit = j;
