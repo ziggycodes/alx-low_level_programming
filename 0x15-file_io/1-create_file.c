26e0ce58b61f0ac2c3ed62f60279ca7e96dcfe25
Isaac Ayodele <ziggycodes25@gmail.com>
2022-12-09 08:15:42 +0100

    file io tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,33 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
+ * append_text_to_file - appends text at the end of a file
  * @filename: filename.
- * @text_content: content writed in the file.
+ * @text_content: added content.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
-int create_file(const char *filename, char *text_content)
+
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nchars;
-	int wrt;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (nchars = 0; text_content[nchars]; nchars++)
-		;
-
-	wrt = write(fd, text_content, nchars);
-
-	if (wrt == -1)
-		return (-1);
-
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+		rwr = write(fd, text_content, nletters);
+		if (rwr == -1)
+			return (-1);
+	}
 	close(fd);
-
 	return (1);
 }
